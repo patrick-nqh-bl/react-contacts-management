76dc2e167f10cb818d1ed88db7368f33e914db29
patrick <patrick204nqh@gmail.com>
2020-05-26 15:21:53 +0700

    add sidebar component & custom add contact button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { Button, Layout, Table } from 'antd';
+import { Button, Layout, Table, Menu, Breadcrumb } from 'antd';
 import { PlusCircleFilled } from '@ant-design/icons';
 import AddDrawer from './AddDrawer';
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
 function App() {
   const [showDrawer, setShowDrawer] = useState(false);
   const [values, setValues] = useState([]);
   const [errorInfo, setErrorInfo] = useState({});
+  const [collapsed, setCollapsed] = useState(false);
+
+  const onCollapse = (isCollapsed) => {
+    setCollapsed(isCollapsed);
+  };
 
   const handleAddFormOnFinish = (data) => {
     setValues([
@@ -46,23 +60,57 @@ function App() {
 
   return (
     <>
-      <Button
-        type='primary'
-        icon={<PlusCircleFilled />}
-        data-testid='add-contact-button'
-        onClick={() => setShowDrawer(true)}
-      >
-        Add
-      </Button>
-      <Layout.Content>
-        <Table dataSource={values} columns={columns}></Table>
-      </Layout.Content>
-      <AddDrawer
-        show={showDrawer}
-        handleOnClose={() => setShowDrawer(false)}
-        handleOnFinish={handleAddFormOnFinish}
-        handleOnFinishFailed={handleAddFormOnFinishFailed}
-      />
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+          <div className='logo' />
+          <Menu theme='dark' defaultSelectedKeys={['1']} mode='inline'></Menu>
+        </Sider>
+        <Layout className='site-layout'>
+          <Header
+            className='site-layout-background'
+            style={{ padding: 0, background: '#fff' }}
+          />
+          <Content style={{ margin: '0 16px' }}>
+            <div
+              className='site-layout-background'
+              style={{ padding: 24, minHeight: 360 }}
+            >
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  marginBottom: 20,
+                }}
+              >
+                <div></div>
+                <div>
+                  <Button
+                    type='primary'
+                    icon={<PlusCircleFilled />}
+                    data-testid='add-contact-button'
+                    onClick={() => setShowDrawer(true)}
+                  >
+                    Add
+                  </Button>
+                </div>
+              </div>
+
+              <Layout.Content>
+                <Table dataSource={values} columns={columns}></Table>
+              </Layout.Content>
+              <AddDrawer
+                show={showDrawer}
+                handleOnClose={() => setShowDrawer(false)}
+                handleOnFinish={handleAddFormOnFinish}
+                handleOnFinishFailed={handleAddFormOnFinishFailed}
+              />
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Ant Design ©2018 Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
     </>
   );
 }
