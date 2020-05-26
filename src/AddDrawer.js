6ccf46debc89496b72c23dbe33068126635524cd
patrick <patrick204nqh@gmail.com>
2020-05-26 11:15:43 +0700

    add AddDrawer component and testing

---
+++ src/AddDrawer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Drawer } from 'antd';
+
+const AddDrawer = ({ show, handleOnClose }) => {
+  return (
+    <Drawer
+      data-testid='add-contact-drawer'
+      title='Add Contact'
+      visible={show}
+      onClose={handleOnClose}
+      maskClosable={false}
+    >
+      <h1>Content Here</h1>
+    </Drawer>
+  );
+};
+
+AddDrawer.propTypes = {
+  show: PropTypes.bool.isRequired,
+  handleOnClose: PropTypes.func.isRequired,
+};
+
+export default AddDrawer;
--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Button } from 'antd';
 import { PlusCircleFilled } from '@ant-design/icons';
+import AddDrawer from './AddDrawer';
 
 function App() {
+  const [showDrawer, setShowDrawer] = useState(false);
   return (
-    <Button
-      type='primary'
-      icon={<PlusCircleFilled />}
-      data-testid='add-contact-button'
-    >
-      Add
-    </Button>
+    <>
+      <Button
+        type='primary'
+        icon={<PlusCircleFilled />}
+        data-testid='add-contact-button'
+        onClick={() => setShowDrawer(true)}
+      >
+        Add
+      </Button>
+      <AddDrawer show={showDrawer} handleOnClose={() => setShowDrawer(false)} />
+    </>
   );
 }
 export default App;
